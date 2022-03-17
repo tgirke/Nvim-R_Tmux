bd4799086267a977faea308c4a8d1305732bb356
Thomas Girke <tgirke@citrus.ucr.edu>
2022-03-16 20:10:41 -0700

    edits

--- Install_Nvim-R_Tmux.sh
+++ Install_Nvim-R_Tmux.sh
@@ -1,53 +1,68 @@
 #!/bin/bash -l 
 
-## Script to install nvim-R-tmux environemnt on HPCC 
+## Script to install Nvim-R_Tmux environemnt on HPCC 
 ## Author: Thomas Girke
-## Date: 20-Apr-18
-## User instructions are here: http://bit.ly/2vvqouy
+## Date: 16-Mar-22
+## User instructions are here: https://bit.ly/3CLzS41
 
-## Check for existence of files/dirs to be created and later deleted 
-if [ -f nvimRtmux.zip ] || [ -d nvimRtmux ]; then
-    printf "File nvimRtmux.zip or directory nvimRtmux exist. Delete or rename them prior to runnng this script. \n"
+## Check in run dir for existence of files/dirs to be created and later deleted 
+if [ -f Install_Nvim-R_Tmux.zip ] || [ -d Install_Nvim-R_Tmux ]; then
+    printf "File Install_Nvim-R_Tmux.zip or directory Install_Nvim-R_Tmux exist. Delete or rename them prior to runnng this script. \n"
     exit 0
 fi
 
-## Download and unzip this file to your home on biocluster
-cd ~/
-cp $(dirname $(realpath $0))/../share/nvimRtmux.zip .
-unzip nvimRtmux.zip > /dev/null 2>&1
+## Clone Install_Nvim-R_Tmux repos to your home on HPCC
+git clone git@github.com:tgirke/Install_Nvim-R_Tmux.git
 
-## Copy the following files/dirs from ~/nvimRtmux and make backups of existing versions
+## Copy the following files/dirs from Install_Nvim-R_Tmux and make backups of existing versions
 ## 1. .bashrc file
-if [ -f .bashrc ]; then
-    mv .bashrc .bashrc_bak
+if [ -f ~/.bashrc ]; then
+    mv ~/.bashrc ~/.bashrc_bak
 fi
-cp nvimRtmux/.bashrc .
-echo "Moved new .bashrc file to home and created backup (.bashrc_bak) of existing one."
+cp Install_Nvim-R_Tmux/.bashrc ~/
+echo "Moved new .bashrc file to home and created backup (~/.bashrc_bak) of existing one."
 
 ## 2. .tmux.conf file
-if [ -f .tmux.conf ]; then
-    mv .tmux.conf .tmux.conf_bak
+if [ -f ~/.tmux.conf ]; then
+    mv ~/.tmux.conf ~/.tmux.conf_bak
 fi
-cp nvimRtmux/.tmux.conf .
-echo "Moved new .tmux.conf file to home and created backup (.tmux.conf_bak) of existing one."
+cp Install_Nvim-R_Tmux/.tmux.conf ~/
+echo "Moved new .tmux.conf file to home and created backup (~/.tmux.conf_bak) of existing one."
 
 ## 3. .Rprofile file
-if [ -f .Rprofile ]; then
-    mv .Rprofile .Rprofile_bak
+if [ -f ~/.Rprofile ]; then
+    mv ~/.Rprofile ~/.Rprofile_bak
 fi
-cp nvimRtmux/.Rprofile .
-echo "Moved new .Rprofile file to home and created backup (.Rprofile_bak) of existing one."
+cp Install_Nvim-R_Tmux/.Rprofile ~/
+echo "Moved new .Rprofile file to home and created backup (~/.Rprofile_bak) of existing one."
 
 ## 4. .config directory
-if [ -f .config ]; then
-    mv .config .config_bak
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config 
 fi
-cp -r nvimRtmux/.config .
-echo "Moved new .config directory to home and created backup (.config_bak) of existing one."
+if [ -d ~/.config/nvim ]; then
+    mv ~/.config/nvim ~/.config/nvim_bak
+fi
+cp -r Install_Nvim-R_Tmux/.config/nvim ~/.config/
+echo "Moved new .config/nvim directory to ~/.config and created backup (~/.config/nvim_bak) of existing one."
+
+## 5. .local directory
+if [ ! -d ~/.local ]; then
+    mkdir ~/.local 
+fi
+if [ ! -d ~/.local/share ]; then
+    mkdir ~/.local/share
+fi
+if [ -d ~/.local/share/nvim ]; then
+    mv ~/.local/share/nvim ~/.local/share/nvim_bak
+fi
+cp -r Install_Nvim-R_Tmux/.local/share/nvim ~/.local/share
+echo "Moved new .local/share/nvim directory to ~/.local/share and created backup (~/.local/share//nvim_bak) of existing one."
 
 ## Clean-up
 #rm nvimRtmux.zip
 #rm -rf nvimRtmux
+rm -rf Install_Nvim-R_Tmux
 
 ## In case someone wants to undo changes here are some instructions
 printf "To undo changes, run: \n\tmv .bashrc .bashrc_bak2; mv .bashrc_bak .bashrc; mv .tmux.conf .tmux.conf_bak2; mv .tmux.conf_bak .tmux.conf; mv .Rprofile .Rprofile_bak2; mv .Rprofile_bak .Rprofile; mv .config .config_bak2; mv .config_bak .config \n"
